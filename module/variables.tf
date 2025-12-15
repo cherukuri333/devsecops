93bef6a90187e5ff512fd5dbfcd2299dc38a5868
cherukuri333 <96185030+cherukuri333@users.noreply.github.com>
2025-12-15 21:01:10 +0530

    Update variables.tf

--- module/variables.tf
+++ module/variables.tf
@@ -1,31 +1,31 @@
-variable "cluster_name" {}
-variable "cidr_block" {}
-variable "vpc_name" {}
+variable "cluster-name" {}
+variable "cidr-block" {}
+variable "vpc-name" {}
 variable "env" {}
-variable "igw_name" {}
-variable "pub_subnet_count" {}
-variable "pub_cidr_block" {
+variable "igw-name" {}
+variable "pub-subnet-count" {}
+variable "pub-cidr-block" {
   type = list(string)
 }
-variable "pub_availability_zone" {
+variable "pub-availability-zone" {
   type = list(string)
 }
-variable "pub_sub_name" {}
-variable "pri_subnet_count" {}
-variable "pri_cidr_block" {
+variable "pub-sub-name" {}
+variable "pri-subnet-count" {}
+variable "pri-cidr-block" {
   type = list(string)
 }
-variable "pri_availability_zone" {
+variable "pri-availability-zone" {
   type = list(string)
 }
-variable "pri_sub_name" {}
-variable "public_rt_name" {}
-variable "private_rt_name" {}
-variable "eip_name" {}
-variable "ngw_name" {}
-variable "eks_sg" {}
+variable "pri-sub-name" {}
+variable "public-rt-name" {}
+variable "private-rt-name" {}
+variable "eip-name" {}
+variable "ngw-name" {}
+variable "eks-sg" {}
 
-# IAM
+#IAM
 variable "is_eks_role_enabled" {
   type = bool
 }
@@ -34,12 +34,10 @@ variable "is_eks_nodegroup_role_enabled" {
 }
 
 # EKS
-variable "is_eks_cluster_enabled" {
-  type = bool
-}
-variable "cluster_version" {}
-variable "endpoint_private_access" {}
-variable "endpoint_public_access" {}
+variable "is-eks-cluster-enabled" {}
+variable "cluster-version" {}
+variable "endpoint-private-access" {}
+variable "endpoint-public-access" {}
 variable "addons" {
   type = list(object({
     name    = string
