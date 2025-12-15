380dfadbdec4f835512b313224872e78c04d47ff
cherukuri333 <96185030+cherukuri333@users.noreply.github.com>
2025-12-15 17:12:49 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "dev-sai-tf-bucket"
+    bucket         = "dev-saii-tf-bucket"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
